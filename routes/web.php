c8100b2bbdf9444fc666c3aad2558f9f54406e44
Danendra AP <danendradhi@gmail.com>
2025-02-18 11:13:33 +0700

    Praktikum 1: Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () { 
+return 'Hello World'; 
+}); 
+
+Route::get('/world', function () { 
+return 'World'; 
+});
+
+Route::get('/', function () { 
+return 'selamat datang'; 
+});
+
+Route::get('/about', function () { 
+return 'Nama : Danendra Adhipramana <br> NIM : 244107023011'; 
+});
+
+Route::get('/user/{name}', function ($name) { 
+return 'Nama saya '.$name; 
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) { 
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='Danen') { 
+    return 'Nama saya '.$name; 
+});
+
